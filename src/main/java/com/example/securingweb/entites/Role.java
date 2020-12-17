aa3b04abbf144cfbf6cb90b076aa011b960f23dc
Ilia <ahumyck@github.com>
2020-12-17 04:15:13 +0400

    roles displaying correctly

--- src/main/java/com/example/securingweb/entites/Role.java
+++ src/main/java/com/example/securingweb/entites/Role.java
@@ -1,29 +1,25 @@
 package com.example.securingweb.entites;
 
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Entity
-@JsonSerialize
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
 public class Role {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "ROLE_ID")
 	private long id;
-
 	private String roleName;
-
-	public Role() {
-	}
 
 	public Role(String roleName) {
 		this.roleName = roleName;
 	}
 
-	@Override
-	public String toString() {
-		return "Role{" + "id=" + id + ", roleName='" + roleName + '\'' + '}';
-	}
 }
--- src/main/java/com/example/securingweb/entites/User.java
+++ src/main/java/com/example/securingweb/entites/User.java
@@ -1,9 +1,18 @@
 package com.example.securingweb.entites;
 
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 import javax.persistence.*;
 
 @Entity
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
 public class User {
 
 	@Id
@@ -19,9 +28,6 @@ public class User {
 	@ManyToOne
 	private Role role;
 
-	public User() {
-	}
-
 	public User(String username, String password, Role role) {
 		this.username = username;
 		this.password = password;
@@ -31,50 +37,5 @@ public class User {
 	public User(String username, String password) {
 		this.username = username;
 		this.password = password;
-	}
-
-	public Role getRole() {
-		return role;
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public String getToken() {
-		return token;
-	}
-
-	public void setRole(Role role) {
-		this.role = role;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public void setToken(String token) {
-		this.token = token;
-	}
-
-	@Override
-	public String toString() {
-		return "User{" + "id=" + id + ", username='" + username + '\'' + ", password='" + password + '\'' + ", role=" + role + '}';
 	}
 }
