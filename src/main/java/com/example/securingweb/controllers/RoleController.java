fe0c200ecfbfb7e2e2a566edca5ff47363e46071
ahumyck <ahumyck@gmail.com>
2020-12-19 20:23:49 +0400

    role controller upd

--- src/main/java/com/example/securingweb/controllers/RoleController.java
+++ src/main/java/com/example/securingweb/controllers/RoleController.java
@@ -2,14 +2,16 @@ package com.example.securingweb.controllers;
 
 import com.example.securingweb.entites.Role;
 import com.example.securingweb.entites.User;
-import com.example.securingweb.security.jwt.JavaWebTokenService;
 import com.example.securingweb.security.cookie.CookieService;
+import com.example.securingweb.security.jwt.JavaWebTokenService;
+import com.example.securingweb.security.userdetails.CustomUserDetails;
 import com.example.securingweb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Optional;
@@ -27,18 +29,17 @@ public class RoleController {
     private CookieService cookieService;
 
     @GetMapping(value = "/user-role")
-    public Optional<Role> getUserRole(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public Optional<Role> getUserRole(HttpServletResponse response) throws IOException {
 
-        Optional<String> cookieFromRequest = cookieService.getCookieFromRequest(request);
-        if (cookieFromRequest.isPresent()) {
-            User user = userService.findByUsername(javaWebTokenService.validateTokenAndGetUsername(cookieFromRequest.get()));
-            if (user == null) {
-                response.sendError(400, "Cannot find user");
-                return Optional.empty();
-            }
-            return Optional.of(user.getRole());
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
+
+        User user = userService.findByUsername(customUserDetails.getUsername());
+        if (user == null) {
+            response.sendError(400, "Cannot find user");
+            return Optional.empty();
         }
-        response.sendError(400, "No cookie, please re-login");
-        return Optional.empty();
+        return Optional.of(user.getRole());
+
     }
 }
